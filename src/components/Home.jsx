48b5c368ccbc12c66c3d1db3e0a5f78431f03bf6
Adithya kC <adithyamh@gmail.com>
2025-02-21 19:46:06 +0530

    added anchor tag to projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div class="hs1">
           {/* <h1>Welcome..!!</h1><br /> */}
           <h1>Hi, my name is <span id="name"><i>Adithya KC</i></span></h1><br />
-          <h1>I m</h1><br />
+          <h1>I am</h1><br />
           <h1 id="typed"><i><ReactTyped
             strings={[
               "Passionate Front-End Developer",
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,45 +8,55 @@ import { Link } from 'react-router-dom'
 const Projects = () => {
   return (
     <div className='projects' id='projectsnav'>
-      <div class="card1" >
-        <div class="front" id='card1'>
-          <div className='blur'>
-            <h3>To-Do-List</h3>
-            {/* <img src={Todolist} alt="to do list" /> */}
+     
+      <a href="https://adithya-bond.github.io/TO_DO_LIST/" target='_blank'>
+        <div class="card1" >
+          <div class="front" id='card1'>
+            <div className='blur'>
+              <h3>To-Do-List</h3>
+              {/* <img src={Todolist} alt="to do list" /> */}
+            </div>
+          </div>
+          <div class="back">
+            <p>
+              This app allows users to add tasks, mark as complete with a tick symbol, and delete tasks. Tasks marked as completed are visually distinguished with a strikethrough. The application features a clean, responsive design with interactive elements for a seamless user experience. Demonstrated proficiency in DOM manipulation, event handling, and front-end web development.
+            </p>
           </div>
         </div>
-        <div class="back">
-          <p>
-             This app allows users to add tasks, mark as complete with a tick symbol, and delete tasks. Tasks marked as completed are visually distinguished with a strikethrough. The application features a clean, responsive design with interactive elements for a seamless user experience. Demonstrated proficiency in DOM manipulation, event handling, and front-end web development.
-
-          </p>
-        </div>
-      </div>
-      <div class="card2" >
-        <div class="front" id='card2'>
-          <div className="blur">
-            <h3>Portfolio</h3>
+      </a>
+      
+      
+      <a href="https://portfolio-67c1.onrender.com" target='_blank'>
+        <div class="card2" >
+          <div class="front" id='card2'>
+            <div className="blur">
+              <h3>Portfolio</h3>
+            </div>
+          </div>
+          <div class="back">
+            <p>
+              This website includes sections such as "About Me",  "Skills",  "Projects",  and "Contact". Implemented modern design principles, smooth navigation, and dynamic content rendering. Demonstrates proficiency in React.js, component-based architecture, and responsive web design.
+            </p>
           </div>
         </div>
-        <div class="back">
-          <p>
-            This website includes sections such as "About Me",  "Skills",  "Projects",  and "Contact". Implemented modern design principles, smooth navigation, and dynamic content rendering. Demonstrates proficiency in React.js, component-based architecture, and responsive web design.
-          </p>
-        </div>
-      </div>
-      <div class="card3" >
-        <div class="front" id='card3'>
-          <div className="blur">
-            <h3>E-Kart</h3>
-            {/* <img src={Ekart} alt="e-kart" /> */}
+      </a>
+      <a href="https://e-commerce-cte9.onrender.com" target='_blank'>
+        <div class="card3" >
+          <div class="front" id='card3'>
+            <div className="blur">
+              <h3>E-Kart</h3>
+              {/* <img src={Ekart} alt="e-kart" /> */}
+            </div>
+          </div>
+          <div class="back">
+            <p>
+              Developed a React.js based e-commerce platform with distinct user and admin sections. Admins can manage products using full CRUD operations, while users can browse and purchase products. Integrated JSON Server for data storage and authentication, enabling secure user and admin login. Features include account creation, role-based access, and a dynamic product catalog. Demonstrates expertise in front-end development, backend integration, and authentication systems.
+            </p>
           </div>
         </div>
-        <div class="back">
-          <p>
-            Developed a React.js based e-commerce platform with distinct user and admin sections. Admins can manage products using full CRUD operations, while users can browse and purchase products. Integrated JSON Server for data storage and authentication, enabling secure user and admin login. Features include account creation, role-based access, and a dynamic product catalog. Demonstrates expertise in front-end development, backend integration, and authentication systems.
-          </p>
-        </div>
-      </div>
+      </a>
+      
+      
 
     </div>
   )
